a00cd44c9395a5ab80827da9bee00314c00e7bf0
hyagoassiz <hyagoassiz@hotmail.com>
2024-11-08 20:22:44 -0300

    GDG-8 feat(Components): adicionado component Container

--- src/App.tsx
+++ src/App.tsx
@@ -5,38 +5,41 @@ import { ToolPainel } from "./app/shared/components/ToolPanel/ToolPanel";
 import { Button } from "@mui/material";
 import { DataTable } from "./app/shared/components/DataTable/DataTable";
 import { Modal } from "./app/shared/components/Modal/Modal";
+import { Container } from "./app/shared/components/Container/Container";
 
 function App() {
   return (
     <>
       <AppBar />
-      {/* <Drawer
+      <Container>
+        {/* <Drawer
         applyFilter={() => alert("")}
         closeFilter={() => alert("")}
         open={true}
       >
         <Box>Teses</Box>
       </Drawer> */}
-      <TitlePage title="Título" subTitle="Sub-Título" />
-      <ToolPainel buttons={<Button>Adicionar</Button>} />
-      <DataTable
-        columns={[{ key: "1", label: "Coluna I" }]}
-        data={[
-          { "1": "Teste", teste: 1 },
-          { "1": "Teste2", teste: 2 },
-          { "1": "Teste3", teste: 3 },
-          { "1": "Teste4", teste: 4 },
-        ]}
-        textForEmptyData="teste"
-      />
-      <Modal
-        buttons={<Button>Fechar</Button>}
-        open={false}
-        style={{ width: 800 }}
-        title="Esse é o título"
-      >
-        Teste
-      </Modal>
+        <TitlePage title="Título" subTitle="Sub-Título" />
+        <ToolPainel buttons={<Button>Adicionar</Button>} />
+        <DataTable
+          columns={[{ key: "1", label: "Coluna I" }]}
+          data={[
+            { "1": "Teste", teste: 1 },
+            { "1": "Teste2", teste: 2 },
+            { "1": "Teste3", teste: 3 },
+            { "1": "Teste4", teste: 4 },
+          ]}
+          textForEmptyData="teste"
+        />
+        <Modal
+          buttons={<Button>Fechar</Button>}
+          open={false}
+          style={{ width: 800 }}
+          title="Esse é o título"
+        >
+          Teste
+        </Modal>
+      </Container>
     </>
   );
 }
---
+++ src/app/shared/components/Container/styles/index.tsx
@@ -0,0 +1,6 @@
+import { Container, styled } from "@mui/material";
+
+export const StyledContainer = styled(Container)(({ theme }) => ({
+  padding: theme.spacing(4),
+  height: "auto",
+}));
