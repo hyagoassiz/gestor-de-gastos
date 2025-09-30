d86cea3f007eb38bd596eb4d4a63df498a865035
hyagoassiz <hyagoassiz@hotmail.com>
2025-09-30 20:46:49 -0300

    interceptando erros do back

--- src/api/constants/api.ts
+++ src/api/constants/api.ts
@@ -1,16 +1,25 @@
 import axios from "axios";
+import { showSnackBar } from "@/redux/snackBarSlice";
+import { store } from "@/redux/store";
 
 export const API = axios.create({
   baseURL: "http://localhost:8080",
 });
 
 API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token"); // token simples, sem JSON.stringify
-
+  const token = localStorage.getItem("token");
   if (token) {
-    config.headers = config.headers || {}; // garante que headers exista
+    config.headers = config.headers || {};
     config.headers["Authorization"] = `Bearer ${token}`;
   }
-
   return config;
 });
+
+API.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const message = error.response?.data?.message || "Erro inesperado";
+    store.dispatch(showSnackBar({ message, type: "error" }));
+    return Promise.reject(error);
+  }
+);
